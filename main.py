7f60deb0d44591904e03e6c592bfdde612fc7154
Ameer <muhammad.ameerasghar10@gmail.com>
2024-10-28 22:41:01 +0000

    Add main.py for flashing LED for testing

---
+++ main.py
@@ -0,0 +1,8 @@
+from time import sleep
+
+# Simulate LED blink pattern
+for _ in range(5):
+    print("LED ON")  # Simulates LED turning on
+    sleep(1)
+    print("LED OFF")  # Simulates LED turning off
+    sleep(1)
